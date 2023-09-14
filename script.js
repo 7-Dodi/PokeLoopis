e56822de739577870db11663802ac5afcffdf994
Diogo Santana <diogosantanafreitasuna@gmail.com>
2023-09-14 17:29:26 -0300

    add:  Checking user response with current Pokémon

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const elPokemonImg = document.getElementsByClassName("pokemon-img-dark")[0];
 
 let pokemonName = "";
 
-fetchData();
 
 async function getPokeData() {
   // gera um numero entre 1 e 500
@@ -19,7 +18,28 @@ async function getPokeData() {
 
 async function fetchData() {
   const { image, name } = await getPokeData();
-
   pokemonName = name;
   elPokemonImg.src = image;
+  console.log(pokemonName)
 }
+
+const form = document.querySelector('form').addEventListener('submit', (eve) => {
+    eve.preventDefault();
+})
+
+
+const sendButton = document.querySelector('#button-poke');
+
+sendButton.addEventListener('click', () => {
+    
+    console.log('Entrou')
+    const container = document.querySelector('.container');
+    const userReponse = document.querySelector('#text-poke').value;
+    if(userReponse === pokemonName) {
+        container.style.backgroundColor = '#26de81'
+        elPokemonImg.style.filter = 'none';
+    }
+
+})
+
+fetchData();
