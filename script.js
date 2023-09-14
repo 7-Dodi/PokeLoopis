9cfbee2bb207c1ed91a96c8d16497cb45bb12506
JoaoPedroli <joaopedrocz12345@gmail.com>
2023-09-14 19:13:47 -0300

    add: reset game

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
-const elPokemonImg = document.getElementsByClassName("pokemon-img-dark")[0];
+const elBody = document.querySelector("body");
+const elPokeImage = document.getElementsByClassName("pokemon-img-dark")[0];
+const elInput = document.querySelector("#text-poke");
+const elButton = document.querySelector("#button-poke");
 
 let pokemonName = "";
 
+handleStart();
+
+async function handleStart() {
+  resetFields();
+
+  const { image, name } = await getPokeData();
+  pokemonName = name;
+  elPokeImage.src = image;
+}
+
+function resetFields() {
+  elBody.style.backgroundColor = "var(--black)";
+  elButton.textContent = "Enviar";
+  elInput.placeholder = "Qual é esse Pokémon?!";
+  elPokeImage.style.filter = "brightness(0)";
+  elInput.disabled = false;
+  elInput.value = "";
+}
 
 async function getPokeData() {
   // gera um numero entre 1 e 500
@@ -13,33 +34,30 @@ async function getPokeData() {
   return {
     image: pokeData.sprites.front_default,
     name: pokeData.name,
-  }
-};
-
-async function fetchData() {
-  const { image, name } = await getPokeData();
-  pokemonName = name;
-  elPokemonImg.src = image;
-  console.log(pokemonName)
+  };
 }
 
-const form = document.querySelector('form').addEventListener('submit', (eve) => {
-    eve.preventDefault();
-})
+const form = document.querySelector("form");
+form.addEventListener("submit", (eve) => {
+  eve.preventDefault();
+});
 
+const sendButton = document.querySelector("#button-poke");
+sendButton.addEventListener("click", () => {
+  if (elButton.textContent === "Reiniciar") {
+    handleStart();
+    return;
+  }
 
-const sendButton = document.querySelector('#button-poke');
+  const userReponse = elInput.value;
 
-sendButton.addEventListener('click', () => {
-    
-    console.log('Entrou')
-    const container = document.querySelector('.container');
-    const userReponse = document.querySelector('#text-poke').value;
-    if(userReponse === pokemonName) {
-        container.style.backgroundColor = '#26de81'
-        elPokemonImg.style.filter = 'none';
-    }
-
-})
-
-fetchData();
+  if (userReponse !== pokemonName) {
+    elBody.style.backgroundColor = "var(--green)";
+    elPokeImage.style.filter = "none";
+    elInput.value = `${
+      pokemonName[0].toUpperCase() + pokemonName.substring(1)
+    }!! Você acertou, Parabéns!!`;
+    elInput.disabled = true;
+    elButton.textContent = "Reiniciar";
+  }
+});
