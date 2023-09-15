8b0b9354e18ff8cd795e5b8fb148beb4ac97aa35
Diogo Santana <diogosantanafreitasuna@gmail.com>
2023-09-15 09:50:28 -0300

    add: Pokemon skip functionality

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+import { typesPokemon } from "./pokemonTypesTranslation.js";
+
+
 const elBody = document.querySelector("body");
 const elPokeImage = document.getElementsByClassName("pokemon-img-dark")[0];
 const elInput = document.querySelector("#text-poke");
@@ -21,99 +24,6 @@ function countMaximumHits() {
 }//Falta adicionar o recorde no localStorage
 /////
 
-//Objeto como a lista de tipos, tradução e a cor correspondente 
-const typesPokemon = [
-  {
-    name: 'fire',
-    tradution: 'Fogo',
-    color: 'var(--light-red)',
-  },
-  {
-    name: 'bug',
-    tradution: 'Inseto',
-    color: 'var(--green)',
-  },
-  {
-    name: 'psychic',
-    tradution: 'Psíquico',
-    color: 'var(--purple)',
-  },
-  {
-    name: 'water',
-    tradution: 'Água',
-    color: 'var(--blue)',
-  },
-  {
-    name: 'ghost',
-    tradution: 'Fantasma',
-    color: 'var(--grey)',
-  },
-  {
-    name: 'normal',
-    tradution: 'Normal',
-    color: 'var(--orange)',
-  },
-  {
-    name: 'ground',
-    tradution: 'Terrestre',
-    color: 'var(--brow)',
-  },
-  {
-    name: 'electric',
-    tradution: 'Elétrico',
-    color: 'var(--yellow)',
-  },
-  {
-    name: 'poison',
-    tradution: 'Venenoso',
-    color: 'var(--magenta)',
-  },
-  {
-    name: 'grass',
-    tradution: 'Planta',
-    color: 'var(--honey)',
-  },
-  {
-    name: 'fighting',
-    tradution: 'Lutador',
-    color: 'var(--naval)',
-  },
-  {
-    name: 'rock',
-    tradution: 'Pedra',
-    color: 'var(--rock)',
-  },
-  {
-    name: 'steel',
-    tradution: 'Aço',
-    color: 'var(--steel)',
-  },
-  {
-    name: 'dragon',
-    tradution: 'Dragão',
-    color: 'var(--kepel)',
-  },
-  {
-    name: 'fairy',
-    tradution: 'Fada',
-    color: 'var(--fairy)',
-  },
-  {
-    name: 'ice',
-    tradution: 'Gelo',
-    color: 'var(--ice)',
-  },
-  {
-    name: 'flying',
-    tradution: 'Voador',
-    color: 'var(--fly)',
-  },
-  {
-    name: 'dark',
-    tradution: 'Sombrio',
-    color: 'var(--dark)',
-  },
-]
 
 //Função para mudar a cor do circulo e da dica
 function colorBackgroundTypes(name){
@@ -214,3 +124,24 @@ sendButton.addEventListener("click", () => {
   }
 });
 
+let numberOfSkips = 0;
+const skipButton = document.querySelector('#skip-button');
+
+skipButton.addEventListener('click', (e) => {
+  numberOfSkips++;
+  if(numberOfSkips <= 3) {
+    handleStart();
+    console.log(numberOfSkips)
+  } 
+  if(numberOfSkips == 3) {
+    skipButton.style.display = 'none';
+  }
+})
+
+skipButton.addEventListener('mouseenter', () => {
+  skipButton.innerText = numberOfSkips
+})
+
+skipButton.addEventListener('mouseout', () => {
+  skipButton.innerText = 'Pular'
+})
